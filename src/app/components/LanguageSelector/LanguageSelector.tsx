4b385915a3e9c4fe398e28a5d226e707474f78ec
GravityTwoG <crytekov@gmail.com>
2024-07-25 13:31:17 +0700

    feat: close languages menu on click outside

--- src/app/components/LanguageSelector/LanguageSelector.tsx
+++ src/app/components/LanguageSelector/LanguageSelector.tsx
@@ -4,11 +4,22 @@ import Globus from '@/ui/icons/Globus.svg?react';
 import ArrowDown from '@/ui/icons/ArrowDown.svg?react';
 import { useState } from 'react';
 import { clsx } from 'clsx';
+import { useClickAway } from '@uidotdev/usehooks';
 
 export type LanguageSelectorProps = {};
 
 export const LanguageSelector = (props: LanguageSelectorProps) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const ref = useClickAway<HTMLUListElement>((e) => {
+    if (e.target instanceof HTMLElement || e.target instanceof SVGElement) {
+      const button = e.target.closest('button');
+      if (button?.classList.contains(classes.LanguageSelectorButton)) {
+        return;
+      }
+    }
+    setIsOpen(false);
+  });
 
   return (
     <div className={classes.LanguageSelector}>
@@ -25,7 +36,7 @@ export const LanguageSelector = (props: LanguageSelectorProps) => {
       </button>
 
       {isOpen && (
-        <ul className={classes.LanguageSelectorDropdown}>
+        <ul ref={ref} className={classes.LanguageSelectorDropdown}>
           <li>
             <button>RU</button>
           </li>
