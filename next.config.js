746de5f548d996a0a05f17e4de2e6e493e7bbd46
GravityTwoG <crytekov@gmail.com>
2024-07-27 19:37:19 +0700

    fix: use default settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // Outputs a Single-Page Application (SPA).
-  distDir: './dist', // Changes the build output directory to `./dist/`.
   webpack(config) {
     config.module.rules.push({
       test: /\.(eot|woff|woff2|ttf|png|jpg|gif)$/,
